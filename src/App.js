138ef1f5854ba9b3b056484aba2663ca2ec12491
Tim Maher <timmaher@192-168-1-100.tpgi.com.au>
2022-08-15 09:46:07 +1000

    change title for testing

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h2>Welcome to Site Gate</h2>
+      <h2>Welcome to Site Gate QR</h2>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
 
@@ -59,7 +59,7 @@ function App() {
          }
 
          if (!!error) {
-           console.log('error');
+           //console.log('error');
          }
         }}
         style={{ width: '80%' }}
